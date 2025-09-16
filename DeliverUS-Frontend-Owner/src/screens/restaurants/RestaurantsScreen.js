9805100ac3eedae2acad4b275c8bb24652688a17
luciiatb04 <luciiatb04@gmail.com>
2025-09-16 16:54:12 +0200

    examen completo

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -40,6 +40,12 @@ export default function RestaurantsScreen ({ navigation, route }) {
           <TextSemiBold>Avg. service time: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.averageServiceMinutes} min.</TextSemiBold></TextSemiBold>
         }
         <TextSemiBold>Shipping: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.shippingCosts.toFixed(2)}€</TextSemiBold></TextSemiBold>
+        { item.percentage > 0 &&
+          <TextSemiBold textStyle={{ color: GlobalStyles.brandGreen }}>¡Incremento de precio aplicado!</TextSemiBold>
+        }
+         {item.percentage < 0 &&
+          <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>¡Descuento aplicado!</TextSemiBold>
+        }
         <View style={styles.actionButtonsContainer}>
           <Pressable
             onPress={() => navigation.navigate('EditRestaurantScreen', { id: item.id })
